9934ad95e336e4b5c016d8db7f52979b940de2cd
JungHun Lee <jhoon2816@gmail.com>
2019-12-17 09:57:43 +0900

    update auth.js && app.js && page.js

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,5 +1,5 @@
 exports.isLoggedIn = (req, res, next) => {
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         next();
     } else {
         res.status(403).send('로그인 필요');
@@ -7,7 +7,7 @@ exports.isLoggedIn = (req, res, next) => {
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
-    if(!req.isAuthenticated()){
+    if (!req.isAuthenticated()) {
         next();
     } else {
         res.redirect('/');
