df7ea9b9513f74f64e6019bd53f5bd91652436e4
JungHun Lee <jhoon2816@gmail.com>
2019-12-17 15:49:47 +0900

    add npm for production && update files

--- passport/facebookStrategy.js
+++ passport/facebookStrategy.js
@@ -1,14 +1,29 @@
 const FacebookStrategy = require('passport-facebook').Strategy;
 
+const { User } = require('../models');
+
 module.exports = (passport) => {
     passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_APP_ID,
         clientSecret: process.env.FACEBOOK_APP_SECRET,
         callbackURL: '/auth/facebook/callback'
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOrCreate({ facebookId: profile.id }, (err, user) => {
-            if (err) { return done(err); }
-            done(null, user);
-        });
-    }))
+    }, async (accessToken, refreshToken, profile, done) => {
+        try{
+          const exUser = await User.findOne({where: { snsId: profile.id, provider: 'facebook'}});
+            if(exUser){
+                done(null, exUser);
+            } else {
+                const newUser = await User.create({
+                    email: profile._json && profile._json.email,
+                    nick: profile.displayName,
+                    snsId: profile.id,
+                    provider: 'facebook',
+                });
+                done(null, newUser);
+            }
+        } catch (error) {
+            console.error(error);
+            done(error);
+        }
+    }));
 };
--- passport/googleStrategy.js
+++ passport/googleStrategy.js
@@ -1,15 +1,29 @@
-const googleStrategy = require('passport-google-oauth2').Strategy;
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 const { User } = require('../models');
 
 module.exports = (passport) => {
-    passport.use(new googleStrategy({
-        clientID: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: '/auth/google/callback',
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOrCreate({ googleId: profile.id }, (err, user) => {
-            return done(err, user);
-        });
+    passport.use(new GoogleStrategy({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback"
+        }, async (accessToken, refreshToken, profile, done) => {
+        try{
+            const exUser = await User.findOne({where: { snsId: profile.id, provider: 'google'}});
+            if(exUser){
+                done(null, exUser);
+            } else {
+                const newUser = await User.create({
+                    email: profile._json && profile._json.email,
+                    nick: profile.displayName,
+                    snsId: profile.id,
+                    provider: 'google',
+                });
+                done(null, newUser);
+            }
+        } catch (error) {
+            console.error(error);
+            done(error);
+        }
     }));
 };
